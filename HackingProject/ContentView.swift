9226f91cf1b24d5cbf4ff231cd6df3591f5fb663
Shawn Roller <sroller@techstyle.com>
2021-11-10 11:11:48 -0500

    Initial Commit

---
+++ HackingProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HackingProject
+//
+//  Created by Shawn Roller on 11/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HackingProject/HackingProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackingProjectApp.swift
+//  HackingProject
+//
+//  Created by Shawn Roller on 11/10/21.
+//
+
+import SwiftUI
+
+@main
+struct HackingProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
